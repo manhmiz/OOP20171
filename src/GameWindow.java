27276042b57df34665c91d169b9670daafe369b1
manhmiz <hungnguyenmanh95@gmail.com>
2017-10-11 20:46:20 +0700

    Added Player Added BackGround

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,15 +1,57 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 public class GameWindow extends JFrame {
 
     GameCanvas canvas;
 
-    public GameWindow(){
-        this.setSize(800, 600);
-        this.setVisible(true);
-        this.setBackground(Color.black);
+    public GameWindow() {
+        this.setSize(400, 600);
         this.canvas = new GameCanvas();
         this.setContentPane(this.canvas);
+        this.addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent windowEvent) {
+
+            }
+
+            @Override
+            public void windowClosing(WindowEvent windowEvent) {
+                System.exit(0);
+
+            }
+
+            @Override
+            public void windowClosed(WindowEvent windowEvent) {
+
+            }
+
+            @Override
+            public void windowIconified(WindowEvent windowEvent) {
+
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent windowEvent) {
+
+            }
+
+            @Override
+            public void windowActivated(WindowEvent windowEvent) {
+
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent windowEvent) {
+
+            }
+        });
+        this.setResizable(false);
+        
+        this.canvas.setVisible(true);
+        this.setVisible(true);
     }
+
 }
